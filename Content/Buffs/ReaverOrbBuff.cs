215250f01692ca4ac7aa29da2b58a66012832eb9
GodzillaJedi Kakar-El <jedikeithdavis@gmail.com>
2026-02-08 01:28:53 -0600

    Readded ReaverOrb

---
+++ Content/Buffs/ReaverOrbBuff.cs
@@ -0,0 +1,41 @@
+﻿using CalamityMod;
+using CalamityMod.CalPlayer;
+using gcsep.Content.Projectiles.Enchantments;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Terraria;
+using Terraria.ModLoader;
+
+namespace gcsep.Content.Buffs
+{
+    public class ReaverOrbBuff : ModBuff
+    {
+        public override void SetStaticDefaults()
+        {
+            Main.buffNoTimeDisplay[base.Type] = true;
+            Main.buffNoSave[base.Type] = true;
+        }
+
+        public override void Update(Player player, ref int buffIndex)
+        {
+            CalamityPlayer calamityPlayer = player.Calamity();
+            GCSEPlayer gCSEPlayer = player.CSE();
+            if (player.ownedProjectileCounts[ModContent.ProjectileType<ReaverOrb>()] > 0)
+            {
+                gCSEPlayer.rOrb = true;
+            }
+            if (!gCSEPlayer.rOrb)
+            {
+                player.DelBuff(buffIndex);
+                buffIndex--;
+            }
+            else
+            {
+                player.buffTime[buffIndex] = 18000;
+            }
+        }
+    }
+}
